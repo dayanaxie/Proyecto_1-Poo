3530dcbaa41c45dc59090c111fbb2df6da34855f
Dayana Xie <xielidayana@gmail.com>
2023-03-21 19:04:43 -0600

    max y minimos configurables en constants

--- proyectoUno/src/Constants/CharConstants.java
+++ proyectoUno/src/Constants/CharConstants.java
@@ -1,10 +1,18 @@
 package Constants;
 
 public class CharConstants {
+    // minimo de energia
+    public static final int MIN_ENERGY = 0;
+    // maximo de energia
+    public static final int MAX_ENERGY = 0;
     // minimo de vision
     public static final int MIN_VISION = 1;
     // maximo de vision
     public static final int MAX_VISION = 6;
+    // minimo de energia
+    public static final int MIN_ENERGIA = 0;
+    // maximo de energia
+    public static final int MAX_ENERGIA = 0;        
     // minimo de velocidad
     public static final int MIN_VELOCIDAD = 1;
     // maximo de velocidad
